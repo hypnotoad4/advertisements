e53b44278490debcbc807643e2605b80e7baf22e
hypnotoad4 <zdima761@gmail.com>
2023-08-18 21:20:27 +0300

    123

--- app_adv/models.py
+++ app_adv/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок", max_length=255)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=20, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+    auction = models.BooleanField("Торг", default=False, help_text="Выберите, будет ли торг уместен")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField("Дата изменения", auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} | {self.title} | {self.price}"
+
+    class Meta:
+        db_table = "advertisements"
