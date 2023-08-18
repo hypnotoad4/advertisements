4671347814c93bc06e0193e72563c919531739bb
hypnotoad4 <zdima761@gmail.com>
2023-08-18 22:29:42 +0300

    123

--- app_adv/admin.py
+++ app_adv/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Advertisement
 
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_false', 'make_auction_true']
+    fieldsets = (
+        ("Общие", {
+            'fields': ('title', 'description')
+        }),
+        ("Финансы", {
+            'fields': ('price', 'auction'),
+            'classes': ('collapse',),
+        })
+    )
+    @admin.action(description='Закрыть торг')
+    def make_auction_false(self, request, queryset):
+        queryset.update(auction=False)
+    @admin.action(description='Открыть торг')
+    def make_auction_true(self, request, queryset):
+        queryset.update(auction=True)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- app_adv/models.py
+++ app_adv/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
 
 class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=255)
@@ -10,7 +12,16 @@ class Advertisement(models.Model):
     updated_at = models.DateTimeField("Дата изменения", auto_now=True)
 
     def __str__(self):
-        return f"{self.id} | {self.title} | {self.price}"
+        return f"id = {self.title} | title = {self.title} | price = {self.price}"
 
     class Meta:
         db_table = "advertisements"
+    
+    @admin.display(description = "Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color:green; font-weight: bold">Сегодня в {}</span>', created_time)
+        else:
+            return self.created_at.strftime("%d.%m.%Y %H:%M:%S")
